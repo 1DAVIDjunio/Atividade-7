8b03362f7b3b2c478bb88b4b7ec417eb94ec83ed
aluno <aluno@LB23-M64.alunos.uniceplac.edu>
2024-05-13 21:16:06 -0300

    commit 7

---
+++ 2bimestre/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        NumerosModel model = new NumerosModel();
+        NumerosView view = new NumerosView();
+        NumerosController controller = new NumerosController(model, view);
+
+        controller.adicionarNumeros();
+        controller.ordenarNumeros();
+        controller.exibirNumeros();
+    }
+}
---
+++ 2bimestre/src/NumerosModel.java
@@ -0,0 +1,15 @@
+public class NumerosModel {
+    private int[] numeros;
+
+    public NumerosModel() {
+        numeros = new int[10];
+    }
+
+    public int[] getNumeros() {
+        return numeros;
+    }
+
+    public void setNumero(int index, int numero) {
+        numeros[index] = numero;
+    }
+}
